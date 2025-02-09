fc21f72c346311696bb63ad138495b724ea1aa4b
Cedrik Hoffmann <dev@choffmann.io>
2025-02-09 21:11:10 +0100

    add exit command

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,20 @@ const std = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
 
     while (true) {
+        var buffer: [1024]u8 = undefined;
         try stdout.print("$ ", .{});
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-        try stdout.print("{s}: command not found\n", .{user_input});
 
-        buffer = undefined;
+        var args = std.mem.splitAny(u8, user_input, " ");
+        const command = args.next().?;
+
+        if (std.mem.startsWith(u8, command, "exit")) {
+            const code = try std.fmt.parseInt(u8, args.next() orelse "0", 10);
+            std.process.exit(code);
+        } else {
+            try stdout.print("{s}: command not found\n", .{user_input});
+        }
     }
 }
